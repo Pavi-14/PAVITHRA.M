093fda9a9dc3a1fa78e93049ceca3cdfe0fc209a
Pavi-14 <pavibecse14@gmail.com>
2016-12-26 11:58:29 +0530

    Add files via upload

---
+++ pn.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int n;
+    printf("\n enter any number: ");
+    scanf("%d",&n);
+    if(n>0)
+     printf("\n positive");
+    else
+        printf("\n negative");
+    getch();
+    return 0;
+    }
