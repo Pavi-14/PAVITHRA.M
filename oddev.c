a0b65b5a9e0ee4b9f8eea9a9a477168a59b89632
Pavi-14 <pavibecse14@gmail.com>
2016-12-26 12:10:01 +0530

    Add files via upload

---
+++ oddev.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n;
+printf("\n enter the number: ");
+scanf("%d",&n);
+if(n%2==0)
+printf("\n even");
+else
+printf("\n odd");
+getch();
+return 0;
+}
