fcc96b7e836b6b0980a1f0db41343920f5e86703
Pavi-14 <pavibecse14@gmail.com>
2016-12-28 13:47:29 +0530

    Add files via upload

---
+++ count.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int n,i,r,sum=0;
+printf("\n enter the number: ");
+scanf("%d",&n);
+while(n!=0)
+{
+r=n%10;
+n=n/10;
+count++
+}
+printf("\n count is:%d",count);
+getch();
+return 0;
+}
